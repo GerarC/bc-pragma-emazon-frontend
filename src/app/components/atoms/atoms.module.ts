f3ba34c3fa8a61d7a7e272327360dd95e57f902f
GerarC <gcastillo.m14@gmail.com>
2024-10-03 12:17:49 -0500

    feat: basic input atom added
    A simple input atom has been added within a change in the styling
    configuration.

--- src/app/components/atoms/atoms.module.ts
+++ src/app/components/atoms/atoms.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { TextComponent } from './text/text.component';
+import { InputAtomComponent } from './input-atom/input-atom.component';
 
 @NgModule({
-    declarations: [ButtonComponent, TextComponent],
+    declarations: [ButtonComponent, TextComponent, InputAtomComponent],
     imports: [CommonModule],
-    exports: [ButtonComponent, TextComponent],
+    exports: [ButtonComponent, TextComponent, InputAtomComponent],
 })
 export class AtomsModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,30 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const host = 'localhost'
+const ports = {
+	user: 8090,
+	stock: 8091,
+	transaction: 8092,
+	cart: 8093,
+	report: 8094,
+}
+
+const basePaths = {
+	user: '/emazon/user/v1',
+	stock: '/emazon/stock/v1',
+	transaction: '/emazon/transaction/v1',
+	cart: '/emazon/cart/v1',
+	report: '/emazon/report/v1',
+}
+
 export const environment = {
-  production: false
+  production: false,
+  userApiUrl: `${host}:${ports.user}${basePaths.user}`,
+  stockApiUrl: `${host}:${ports.stock}${basePaths.stock}`,
+  transactionApiUrl: `${host}:${ports.transaction}${basePaths.transaction}`,
+  cartApiUrl: `${host}:${ports.cart}${basePaths.cart}`,
+  reportApiUrl: `${host}:${ports.report}${basePaths.report}`,
 };
 
 /*
